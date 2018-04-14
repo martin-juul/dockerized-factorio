3afef1f31145a04b0b261130f0b4d87149221637
Martin <mart071f@edu.sde.dk>
2018-04-14 13:31:35 +0200

    [docker-entrypoint.sh]: Executing world creation in a sub shell. To prevent it from exiting the script.

--- stable/docker-entrypoint.sh
+++ stable/docker-entrypoint.sh
@@ -10,7 +10,8 @@ if [ -f /tmp/install-factorio.sh ]; then
     /tmp/install-factorio.sh
     rm /tmp/install-factorio.sh
     mv /tmp/server-settings.json /srv/dockerized-factorio/server/server-settings.json
-    exec su-exec factorio:factorio /srv/dockerized-factorio/server/bin/x64/factorio --create /srv/dockerized-factorio/server/data/saves/dockerized-factorio-world.zip
+    (exec su-exec factorio:factorio /srv/dockerized-factorio/server/bin/x64/factorio --create /srv/dockerized-factorio/server/data/saves/dockerized-factorio-world.zip)
+    echo "Finished installing the server!"
 fi
 
 run_factorio_server
