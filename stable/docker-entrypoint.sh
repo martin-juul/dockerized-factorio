10c7e3555cf5bb5db4e863220580f17a52d24d29
Martin <mart071f@edu.sde.dk>
2018-04-14 12:54:08 +0200

    [docker-entrypoint]: Changed save path to a more reasonable one.

--- stable/docker-entrypoint.sh
+++ stable/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 run_factorio_server() {
     cd '/srv/dockerized-factorio/server/bin/x64'
-    exec su-exec factorio:factorio ./factorio --start-server ../../saves/my-save.zip --server-settings ../../server-settings.json
+    exec su-exec factorio:factorio ./factorio --start-server ../../data/saves/dockerized-factorio-world.zip --server-settings ../../server-settings.json
 }
 
 if [ -f /tmp/install-factorio.sh ]; then
@@ -10,7 +10,7 @@ if [ -f /tmp/install-factorio.sh ]; then
     /tmp/install-factorio.sh ${factorio_archive}
     rm /tmp/install-factorio.sh
     mv /tmp/server-settings.json /srv/dockerized-factorio/server/server-settings.json
-    exec su-exec factorio:factorio /srv/dockerized-factorio/server/bin/x64/factorio --create /srv/dockerized-factorio/server/saves/my-save.zip
+    exec su-exec factorio:factorio /srv/dockerized-factorio/server/bin/x64/factorio --create /srv/dockerized-factorio/server/data/saves/dockerized-factorio-world.zip
 fi
 
 run_factorio_server
